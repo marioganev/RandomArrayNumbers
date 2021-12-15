11d891e6a002227e38937c5107ece0592afd479b
User 14 <User 14@PC15>
2021-12-15 08:14:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace random_zada4a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[35];
+            Random r = new Random();
+
+            for (int i = 0; i < 35; i++)
+            {
+                int rand = r.Next(0, 100);
+                arr[i] = rand;
+
+                Console.WriteLine(arr[i]);
+            }
+        }
+        
+    }
+}
